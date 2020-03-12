95d419481f464d141d145bfd04e2521e6c6d152e
Hector Diaz <thegreatcarradio@yahoo.com>
2020-03-11 20:58:55 -0600

    most of the mobile view is working as expected.
    Need to tweek tablet veiw. the topbar is ambiguious in that view. scrapping the text squeezed right next to the image. then it's on to building interior pages.

--- mockup/public/js/scripts.js
+++ mockup/public/js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+
+ /* -- variables  -- */
+	const viewport = $(window);
+ /* -- end variables  -- */
+
+	/* -- dev function  -- */
 	$('a').click(function(e){
 		e.preventDefault();
 	});
+	/* -- end dev function  -- */
 
 	$('#nav .numberd>a').hover(function(e){
 		const data = this; 
@@ -12,4 +19,34 @@ $(document).ready(function(){
 		$(`${img}, ${sum}`).addClass('show');
 		$(data).parent('.numberd').addClass('show');
 	});
+
+ /* -- mobile view functions  -- */
+/// topbar nav
+
+	// open/close nav menu
+	$('#hamburger').click(function(){
+		const icon = this;
+		const nav = $(this).next('.nav');
+		nav.toggleClass('show');
+	});
+
+	// open/close submenu
+	$('.nav a').click(function(){
+		const submenu = $( this ).next('.submenu');
+		const width = viewport.width();
+		const open = $('.submenu.show');
+		// check if other sumenues are open
+		if (open != 'undefined'){
+			// if so close them.
+			open.toggleClass('show');
+			// also closes submenues when another a is clicked
+		}
+		if(submenu != 'undefined' && width < 768) {
+			$(submenu).toggleClass('show');
+		}
+	});
+
+/// end topbar
+ /* -- end mobile view functions  -- */
+
 });
